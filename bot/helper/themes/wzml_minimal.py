5af41e449c1a59dfe547dbf074afb344fc1892de
Kamlesh Kumar Saini <131683941+BrothersIndia7@users.noreply.github.com>
2023-06-18 06:54:34 +0530

    Update wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -3,9 +3,9 @@ class WZMLStyle:
     # ----------------------
     # async def start(client, message) ---> __main__.py
     ST_BN1_NAME = 'Repo'
-    ST_BN1_URL = 'https://www.github.com/weebzone/WZML-X'
+    ST_BN1_URL = 'https://www.github.com/oVo-HxBots'
     ST_BN2_NAME = 'Updates'
-    ST_BN2_URL = 'https://t.me/WZML_X'
+    ST_BN2_URL = 'https://t.me/HxSupport'
     ST_MSG = '''This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram.
 Type {help_command} to get a list of available commands'''
     ST_BOTPM = '''Now, This bot will send all your files and links here. Start Using ...'''
